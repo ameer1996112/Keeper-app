ee8be51dc43f4440f1ad806332e89126114beeeb
Ameer <ameer_1996112@hotmail.com>
2021-08-25 19:56:13 +0300

    first commit

---
+++ build/precache-manifest.2907933e085b6763d72eb1b6d9438a9e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de4eacbe626c4252b0b3ec5c3b826aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "fa8a00eecbc326fe6a3f",
+    "url": "/static/js/main.98ab5968.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
